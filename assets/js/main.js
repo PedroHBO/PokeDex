265a7c75a5c47001d774949bf2f6c471471b8117
Pedro Henrique <pedro.hbo@hotmail.com>
2024-01-20 09:21:25 -0300

    Ajustando pokeApi.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon) {
-    return `
+  return `
         <li class="pokemon">
             <span class="number">001</span>
             <span class="name">${pokemon.name}</span>
@@ -16,20 +12,11 @@ function convertPokemonToLi(pokemon) {
                     alt="${pokemon.name}">
             </div>
         </li>
-    `
+    `;
 }
 
-const pokemonList = document.getElementById('pokemonList')
+const pokemonList = document.getElementById("pokemonList");
 
-fetch(url)
-    .then((response) => response.json())
-    .then((responseBody) => responseBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-
-        }
-    })
-    .catch((error) => console.error(error))
-
+pokeApi.getPokemons().then((pokemons = []) => {
+  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
+});

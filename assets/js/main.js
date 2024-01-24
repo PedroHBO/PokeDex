80e7d55eaf7eebdd7b8277c0242d91f39adbcc1a
TI Civil <pedro.oliveira@civil.com.br>
2024-01-24 09:08:14 -0300

    feature: adicionando os details da API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToLi(pokemon) {
-  return `
+    return `
         <li class="pokemon">
-            <span class="number">001</span>
+            <span class="number">#${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    <li class="type">Grass</li>
-                    <li class="type">Poison</li>
+                ${convertPokemonTypesToLi(pokemon.types).join(' ')}
                 </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+            <img src="${pokemon.sprites.other.dream_world.front_default}"
                     alt="${pokemon.name}">
             </div>
         </li>
@@ -18,5 +21,5 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById("pokemonList");
 
 pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
 });
